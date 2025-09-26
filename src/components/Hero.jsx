62786e9f6816592aec0fec5acec41775a78bf71a
Hamza-Shahid10 <haamzaad01@gmail.com>
2025-09-26 12:12:30 +0500

    Update: daily changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="hero-content" data-aos="fade-up">
-        <h1>Feel The Handmade luxury</h1>
-        <p className="lead">Timeless comfort, rugged elegance. Designed to last a lifetime.</p>
+        {/* <h1>Feel The Handmade luxury</h1>
+        <p className="lead">Timeless comfort, rugged elegance. Designed to last a lifetime.</p> */}
         <Link to="/category/men" className="hero-banner-btnn">
           View Collection
         </Link>

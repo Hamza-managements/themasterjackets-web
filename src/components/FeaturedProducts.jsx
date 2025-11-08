588187f808fd0f0f33960088f6e8c74dc292fee8
Hamza-Shahid10 <haamzaad01@gmail.com>
2025-11-08 14:46:39 +0500

    Update: daily changes

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './styles/style.css';
 import { useNavigate } from 'react-router-dom';
 import { getProducts } from '../utils/ProductServices';

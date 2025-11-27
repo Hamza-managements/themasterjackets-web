4432a0d9f94cb75104f87669073f853304e34351
Hamza Shahid <haamzaad01@gmail.com>
2025-11-27 13:59:51 +0500

    Daily auto update - 27/11/2025 13:59:51.25

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -34,7 +34,7 @@ export default function FeaturedProducts({ title }) {
                         product?.attributes?.badge?.toLowerCase() === "best seller" ||
                         product?.attributes?.badge?.toLowerCase() === "bestseller"
                 );
-                const featured = bestSellers.slice(0, 12);
+                const featured = bestSellers.slice(0, 16);
                 setProducts(featured);
                 setSortedProducts(featured);
 
